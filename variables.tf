5f7a001e9c7680945027e943c38676ec5b230279
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-06-18 16:16:01 +0100

    Add dev-tools VPC

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "assume_role" {
+  type    = string
+  default = "ci"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
---
+++ vpc.tf
@@ -0,0 +1,36 @@
+module "dev_tools_vpc" {
+  source                                     = "dwp/vpc/aws"
+  version                                    = "2.0.12"
+  vpc_name                                   = "dev-tools"
+  region                                     = var.region
+  vpc_cidr_block                             = local.cidr_block[local.environment]["dev-tools-vpc"]
+  gateway_vpce_route_table_ids               = []
+  interface_vpce_source_security_group_count = 0
+  interface_vpce_source_security_group_ids   = []
+  interface_vpce_subnet_ids                  = aws_subnet.public.*.id
+  common_tags                                = local.common_tags
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = module.dev_tools_vpc.vpc.id
+  tags   = merge(local.common_tags, { Name = local.name })
+}
+
+resource "aws_subnet" "public" {
+  count                   = local.zone_count
+  cidr_block              = cidrsubnet(module.dev_tools_vpc.vpc.cidr_block, 2, count.index)
+  vpc_id                  = module.dev_tools_vpc.vpc.id
+  availability_zone_id    = data.aws_availability_zones.current.zone_ids[count.index]
+  map_public_ip_on_launch = true
+  tags                    = merge(local.common_tags, { Name = "${local.name}-public-${local.zone_names[count.index]}" })
+}
+
+resource "aws_default_route_table" "public" {
+  default_route_table_id = module.dev_tools_vpc.vpc.main_route_table_id
+  tags                   = merge(local.common_tags, { Name = "${local.name}-public" })
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
